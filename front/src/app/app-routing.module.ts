495dbc80b36944c39905f22d74ac6dceda795d58
heypollux <paoladepaula@icloud.com>
2025-10-08 11:18:03 -0300

    feat: add routing for Home and update Cabecalho component for navigation

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContatoComponent } from './components/contato/contato.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'contato',
     component: ContatoComponent
-    
   }
 ];
 
