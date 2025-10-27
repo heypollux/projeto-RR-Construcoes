e65f927465a1954ad403579fd8da3268e4e20ca4
JoaoVitor615 <jvpeniza@gmail.com>
2025-10-26 23:31:16 -0300

    feat: implements slider in home page

--- front/src/app/classes/Projeto.ts
+++ front/src/app/classes/Projeto.ts
@@ -2,6 +2,6 @@ export default class Projeto {
   titulo: string;
   descricao: string;
   link: string;
-  caminho_pasta: string;
+  caminho_pasta?: string;
   assets: string[];
 }
--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
-import { Component, HostListener, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef, OnInit, OnDestroy } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
-
+import { ProjetosService } from '../../services/projetos.service';
+import Projeto from '../../classes/Projeto';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   @ViewChild('container1Ref') container1Ref!: ElementRef;
   @ViewChild('container2Ref') container2Ref!: ElementRef;
@@ -16,12 +18,90 @@ export class HomeComponent implements OnInit {
   @ViewChild('container5Ref') container5Ref!: ElementRef;
   
   windowHeight: number = 0;
+  projetos: Projeto[] = [];
+  currentSlideIndex: number = 0;
+  private autoPlayInterval: any;
+  private projetosSubscription!: Subscription;
 
-  constructor(private scroller: ViewportScroller) { }
+  constructor(
+    private scroller: ViewportScroller,
+    private projetosService: ProjetosService
+  ) { }
 
   ngOnInit(): void {
     this.windowHeight = window.innerHeight;
     setTimeout(() => this.checkVisibility(), 0); 
+    this.carregarProjetos();
+  }
+
+  ngOnDestroy(): void {
+    if (this.autoPlayInterval) {
+      clearInterval(this.autoPlayInterval);
+    }
+    if (this.projetosSubscription) {
+      this.projetosSubscription.unsubscribe();
+    }
+  }
+
+  carregarProjetos(): void {
+    this.projetosSubscription = this.projetosService.getProjetos().subscribe({
+      next: (projetos) => {
+        this.projetos = projetos;
+        this.startAutoPlay();
+      },
+      error: (error) => {
+        console.error('Erro ao carregar projetos:', error);
+      }
+    });
+  }
+
+  startAutoPlay(): void {
+    if (this.autoPlayInterval) {
+      clearInterval(this.autoPlayInterval);
+    }
+    this.autoPlayInterval = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  stopAutoPlay(): void {
+    if (this.autoPlayInterval) {
+      clearInterval(this.autoPlayInterval);
+      this.autoPlayInterval = null;
+    }
+  }
+
+  nextSlide(): void {
+    if (this.projetos.length > 0) {
+      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.projetos.length;
+    }
+  }
+
+  prevSlide(): void {
+    if (this.projetos.length > 0) {
+      this.currentSlideIndex = (this.currentSlideIndex - 1 + this.projetos.length) % this.projetos.length;
+    }
+  }
+
+  goToSlide(index: number): void {
+    this.currentSlideIndex = index;
+    this.startAutoPlay();
+  }
+
+  getCurrentProjeto(): Projeto | undefined {
+    return this.projetos[this.currentSlideIndex];
+  }
+
+  getCurrentImage(): string {
+    const projeto = this.getCurrentProjeto();
+    return projeto?.assets?.[0] || '';
+  }
+
+  onImageLoad(event: Event): void {
+    const img = event.target as HTMLImageElement;
+    if (img) {
+      img.style.opacity = '1';
+    }
   }
 
   @HostListener('window:scroll', [])
